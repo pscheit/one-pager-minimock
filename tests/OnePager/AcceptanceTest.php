7ba6405454e8e5d95dea2c696f08214b9067f916
Philipp Scheit <p.scheit@ps-webforge.com>
2013-10-11 14:10:58 +0200

    add the minimock-boilerplate

---
+++ tests/OnePager/AcceptanceTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OnePager;
+
+use Webforge\Code\Test\GuzzleTester;
+use Webforge\Code\Test\HTMLTesting;
+
+class AcceptanceTest extends \Webforge\Code\Test\Base implements HTMLTesting {
+  
+  public function setUp() {
+    parent::setUp();
+
+    $url = 'http://one-pager-minimock.desktop.ps-webforge.net/';
+
+    $this->guzzle = new GuzzleTester($url);
+    $this->guzzle->setDefaultAuth('user', 'secret');
+  }
+
+  public function testThatTheMustacheTemplateEqualsTheHTMLTemplate() {
+    $this->guzzle->get('/');
+
+    $response = $this->guzzle->dispatch();
+
+    $this->html = (string) $response->getBody();
+
+    $this->css('html')->count(1)
+      ->css('body')->count(1)
+        ->css('nav.navbar')->count(1)->end()
+        ->css('div.container.features')->count(1)
+          ->css('.featurette:eq(0)')->count(1)->hasAttribute('id', 'about')
+            ->css('img')->hasClass('pull-right')->end()
+          ->end()
+          ->css('.featurette:eq(1)')->count(1)->hasAttribute('id', 'services')
+            ->css('img')->hasClass('pull-left')->end()
+          ->end()
+          ->css('.featurette:eq(2)')->count(1)->hasAttribute('id', 'contact')
+            ->css('img')->hasClass('pull-right')->end()
+          ->end()
+          ->end()
+        ->end()
+    ;
+  }
+}
---
+++ www/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use Webforge\Minimock\Container;
+
+$boot = require __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'bootstrap.php';
+$container = new Container($boot->getPackage(), $boot->getWebforge()->getVendorPackage('webforge/minimock'));
+$engine = $container->getTemplateEngine();
+
+print $engine->render('one-pager/layout', (object) array(
+  'features'=>array(
+    (object) array(
+      'id'=>'about',
+      'img'=>array(
+        'src'=>'http://placecreature.com/500/500',
+        'class'=>'pull-right'
+       ),
+      'headline'=>'This First Heading',
+      'caption'=>'Will Catch Your Eye',
+      'text'>'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
+     ),
+
+    (object) array(
+      'id'=>'services',
+      'img'=>array(
+        'src'=>'http://placecreature.com/500/500',
+        'class'=>'pull-left'
+      ),
+      'headline'=>'The Second Heading',
+      'caption'=>'Is Pretty Cool Too.',
+      'text'>'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
+    ),
+
+    (object) array(
+      'id'=>'contact',
+      'img'=>array(
+        'src'=>'http://placecreature.com/500/500',
+        'class'=>'pull-right'
+      ),
+      'headline'=>'The Third Heading',
+      'caption'=>'Will Seal the Deal.',
+      'text'=>'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
+    )
+  )
+));
