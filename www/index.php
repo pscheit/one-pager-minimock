eb980d2c17931d95ac174484a3b68d07e93ae0eb
Philipp Scheit <p.scheit@ps-webforge.com>
2013-10-11 14:16:58 +0200

    fix typo in model and add json representation

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@ $boot = require __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'bootstrap.
 $container = new Container($boot->getPackage(), $boot->getWebforge()->getVendorPackage('webforge/minimock'));
 $engine = $container->getTemplateEngine();
 
-print $engine->render('one-pager/layout', (object) array(
+print $engine->render('one-pager/layout', $model = (object) array(
   'features'=>array(
     (object) array(
       'id'=>'about',
@@ -16,7 +16,7 @@ print $engine->render('one-pager/layout', (object) array(
        ),
       'headline'=>'This First Heading',
       'caption'=>'Will Catch Your Eye',
-      'text'>'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
+      'text'=>'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
      ),
 
     (object) array(
@@ -27,7 +27,7 @@ print $engine->render('one-pager/layout', (object) array(
       ),
       'headline'=>'The Second Heading',
       'caption'=>'Is Pretty Cool Too.',
-      'text'>'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
+      'text'=>'Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.'
     ),
 
     (object) array(
